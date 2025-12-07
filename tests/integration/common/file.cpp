d0b9640a79a4ea7c079564f60e8e43695ecc9194
David Ward <dw@mega.co.nz>
2025-12-08 10:37:43 +1300

    We can now generate a random file of a specific length.

--- tests/integration/common/file.cpp
+++ tests/integration/common/file.cpp
@@ -1,7 +1,9 @@
 #include <mega/common/testing/file.h>
+#include <mega/common/testing/utility.h>
 #include <mega/logging.h>
 
 #include <fstream>
+#include <utility>
 
 namespace mega
 {
@@ -30,6 +32,42 @@ File::File(const std::string& content, const std::string& name, const Path& pare
 
 File::File(const std::string& content, const std::string& name):
     File(content, name, fs::current_path())
+{}
+
+File::File(std::uint64_t length, const std::string& name, const Path& parentPath):
+    mPath(parentPath.path() / fs::u8path(name))
+{
+    // So we can create a new file on disk.
+    std::ofstream ostream;
+
+    // So IO failures result in an exception.
+    ostream.exceptions(std::ios::badbit | std::ios::failbit);
+
+    // Open our file for writing.
+    ostream.open(mPath.string(), std::ios::binary | std::ios::trunc);
+
+    // We'll write data to the file in chunks up to 1MiB.
+    auto chunkSize = std::min<std::uint64_t>(1ul << 20, length);
+
+    // Generate a chunk of random bytes.
+    auto chunk = randomBytes(chunkSize);
+
+    // Generate the file by repeatedly writing chunks to disk.
+    for (; length > chunkSize; length -= chunkSize)
+        ostream.write(chunk.data(), static_cast<std::streamsize>(chunkSize));
+
+    // Write any remaining bytes to disk.
+    //
+    // Necessary when length isn't a multiple of chunkSize.
+    if (length)
+        ostream.write(chunk.data(), static_cast<std::streamsize>(length));
+
+    // Flush content to disk.
+    ostream.flush();
+}
+
+File::File(std::uint64_t length, const std::string& name):
+    File(length, name, fs::current_path())
 {}
 
 File::~File()
