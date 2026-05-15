93a9b1381f5de48c7ca5ff672d58ba9a9bbdebd0
Dmitro Baryshev <dba@mega.co.nz>
2026-05-15 10:00:33 +0300

    Move makeMountException() body to .cpp

--- include/mega/fuse/common/mount_exception.h
+++ include/mega/fuse/common/mount_exception.h
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <mega/common/utility.h>
-#include <mega/fuse/common/logger.h>
 #include <mega/fuse/common/mount_result.h>
 
-#include <cerrno>
-#include <cstdarg>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -85,36 +81,8 @@ public:
  * @return
  * Fully initialized mount exception.
  */
-inline MountException
-    makeMountException(int errorCode, const char* file, unsigned int line, const char* format, ...)
-{
-    std::va_list arguments;
-    va_start(arguments, format);
-    auto message = common::formatv(arguments, format);
-    va_end(arguments);
-
-    logger().error(file, "%s", line, message.c_str());
-
-    auto toMountResult = [](int errorCode) -> MountResult
-    {
-#ifdef _WIN32
-        (void)errorCode;
-        return MOUNT_UNEXPECTED;
-#else
-        switch (errorCode)
-        {
-            case ENOTCONN:
-            case ESTALE:
-                return MOUNT_LOCAL_STALE;
-            default:
-                return MOUNT_UNEXPECTED;
-        }
-#endif
-    };
-
-    auto result = toMountResult(errorCode);
-    return MountException(result, errorCode, std::move(message));
-}
+MountException
+    makeMountException(int errorCode, const char* file, unsigned int line, const char* format, ...);
 
 #define FUSEMountError1(errorCode, format) \
     ::mega::fuse::makeMountException((errorCode), __FILE__, __LINE__, (format))
